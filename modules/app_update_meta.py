a752dcf33aadeac945e759d1b924c7296730b389
Stefan Tapper <tapper.stefan@gmail.com>
2018-11-27 15:03:06 +0100

    add option to edit dpi meta data

--- modules/app_update_meta.py
+++ modules/app_update_meta.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from PyQt5.QtWidgets import QTreeWidgetItem
 from PyQt5.QtCore import QObject, QTimer, pyqtSignal, QThread
 
-from modules.widgets.message_box import GenericMsgBox
+from modules.widgets.message_box import GenericMsgBox, QuestionBox
 from modules.exif_worker import Exif
 from modules.detect_language import get_translation
 from modules.log import init_logging
@@ -18,27 +18,74 @@ _ = lang.gettext
 
 
 class ImgMetaDataApp(QObject):
+    update_dpi = False
+    dpi_res_x = '300.0'
+    dpi_res_y = '300.0'
+    dpi_unit = '2'  # 3=cm 2=inches
+    dpi_tags = ['-YResolution', '-XResolution', '-ResolutionUnit']
+
     def __init__(self, ui):
         super(ImgMetaDataApp, self).__init__(ui)
         self.ui = ui
 
         self.exif_worker = None
+        self.update_from_excel = True
 
         self.ui.startBtn.pressed.connect(self.start_exif_worker)
 
     def start_exif_worker(self):
         self.ui.tabWidget.setCurrentIndex(0)
         self.ui.startBtn.setEnabled(False)
+
+        if not self.check_data():
+            self.finish_exif_worker()
+            return
+
         self.run_exif_app()
 
     def finish_exif_worker(self):
         self.ui.tabWidget.setCurrentIndex(1)
-        self.exif_worker.exit()
+
+        if self.exif_worker:
+            self.exif_worker.exit()
+
         self.ui.startBtn.setEnabled(True)
         self.ui.progress_widget.progress.hide()
 
+    def check_data(self):
+        if not self.ui.img_dir.path or not self.ui.img_dir.path.exists():
+            LOGGER.error('Can not start img process. Missing images path.')
+            self.message(_('Kein Pfad zu Bilddaten angegeben.'))
+            return False
+
+        self.update_from_excel = True
+        if not self.ui.excel_data:
+            question = self.ask_proceed_without_excel(self.ui)
+            if question:
+                # User wants to proceed without excel data
+                self.update_from_excel = False
+                if not self.update_dpi:
+                    self.message(_('Weder DPI noch Excel Daten zum aktualisieren ausgewählt.<br><br>'
+                                   'Der Vorgang wird abgebrochen.'))
+                    return False
+
+                LOGGER.error('No excel data. But user wants to continue.')
+                return True
+
+            LOGGER.error('Can not start img process. No excel data.')
+            return False
+
+        if not self.verify_excel_data(self.ui.excel_data):
+            LOGGER.error('Excel contained invalid keys only. Could not associate image files. Aborting.')
+            self.message(_('Der Tabelle fehlen Daten in der Spalte für die korrespondierenden Dateinamen.<br><br>'
+                            'Konnte keine Einträge mit gültigen Dateinamen finden. Überprüfe in den Einstellungen '
+                            'ob die Spalte für die Dateinamen korrekt eingestellt ist.'))
+            return False
+
+        return True
+
     def run_exif_app(self):
-        self.exif_worker = ImgMetaDataWorker(self.ui.img_dir.path, self.ui.excel_data)
+        self.exif_worker = ImgMetaDataWorker(self, self.ui.img_dir.path, self.ui.excel_data, self.update_from_excel)
 
         self.exif_worker.num_items.connect(self.setup_progress)
         self.exif_worker.result.connect(self.update_progress)
@@ -65,6 +112,31 @@ class ImgMetaDataApp(QObject):
         msg_box.exec()
         self.ui.statusBar().showMessage(msg, 8000)
 
+    @staticmethod
+    def ask_proceed_without_excel(ui):
+        question = QuestionBox.ask(parent=ui, title_txt=_('Bilddaten aktualisieren'),
+                                   message=_('Es wurden keine Exceldaten geladen. '
+                                             'Es werden keine Metadaten aktualisiert!'
+                                             '<br><br>Möchtest du trotzedem fortfahren?'), ok_btn_txt=_('Fortfahren'),
+                                   abort_btn_txt=_('Abbrechen'))
+        return question
+
+    @staticmethod
+    def verify_excel_data(excel_data: dict):
+        invalid_keys = set()
+
+        for k in excel_data.keys():
+            if not k:
+                invalid_keys.add(k)
+
+        for k in invalid_keys:
+            excel_data.pop(k)
+
+        if not excel_data:
+            return False
+
+        return True
+
 
 class ImgMetaDataWorker(QThread):
     result = pyqtSignal(str, str)
@@ -72,10 +144,19 @@ class ImgMetaDataWorker(QThread):
     msg = pyqtSignal(str)
     finish_process = pyqtSignal()
 
-    def __init__(self, path: Union[Path, None], excel_data):
+    def __init__(self, parent_app: ImgMetaDataApp, path: Union[Path, None], excel_data, update_from_excel: bool):
         super(ImgMetaDataWorker, self).__init__()
+
+        # Store dpi settings
+        self.update_dpi = parent_app.update_dpi
+        self.dpi_tags = parent_app.dpi_tags
+        self.dpi_unit = parent_app.dpi_unit
+        self.dpi_res_x = parent_app.dpi_res_x
+        self.dpi_res_y = parent_app.dpi_res_y
+
         self.path = path
         self.excel_data = excel_data
+        self.update_from_excel = update_from_excel
 
         self.exif = Exif(self.path, self.idealThreadCount())
         self.exif.result.connect(self.exif_result)
@@ -96,10 +177,12 @@ class ImgMetaDataWorker(QThread):
         LOGGER.debug('Img Worker Thread ended.')
 
     def init_process(self):
-        if not self.check_data():
-            return
-
         for img_file in self.exif.get_img_files():
+            if not self.update_from_excel:
+                # No excel data to update
+                self.img_work_queue.append((img_file, None))
+                continue
+
             # Last two digits of file name indicate page number and are ignored
             file_match = fnmatch.filter(self.excel_data.keys(), f'{img_file.stem[:-2]}??')
 
@@ -115,45 +198,6 @@ class ImgMetaDataWorker(QThread):
         self.num_items.emit(len(self.img_work_queue))
         self.work()
 
-    def check_data(self):
-        if not self.path or not self.path.exists():
-            LOGGER.error('Can not start img process. Missing images path.')
-            self.msg.emit(_('Kein Pfad zu Bilddaten angegeben.'))
-            self.finish_process.emit()
-            return False
-
-        if not self.excel_data:
-            LOGGER.error('Can not start img process. No excel data.')
-            self.msg.emit(_('Keine Excel Daten geladen.'))
-            self.finish_process.emit()
-            return False
-
-        if not self.verify_excel_data(self.excel_data):
-            LOGGER.error('Excel contained invalid keys only. Could not associate image files. Aborting.')
-            self.msg.emit(_('Der Tabelle fehlen Daten in der Spalte für die korrespondierenden Dateinamen.<br><br>'
-                            'Konnte keine Einträge mit gültigen Dateinamen finden. Überprüfe in den Einstellungen '
-                            'ob die Spalte für die Dateinamen korrekt eingestellt ist.'))
-            self.finish_process.emit()
-            return False
-
-        return True
-
-    @staticmethod
-    def verify_excel_data(excel_data: dict):
-        invalid_keys = set()
-
-        for k in excel_data.keys():
-            if not k:
-                invalid_keys.add(k)
-
-        for k in invalid_keys:
-            excel_data.pop(k)
-
-        if not excel_data:
-            return False
-
-        return True
-
     def work(self):
         if not len(self.img_work_queue):
             self.finish_work()
@@ -162,11 +206,17 @@ class ImgMetaDataWorker(QThread):
         img_file, img_dict = self.img_work_queue.pop(0)
         command = list()
 
-        for tag, dict_value in zip(Exif.exiftool_tags, img_dict.values()):
-            if not dict_value:
-                continue
+        if self.update_dpi:
+            # Update units from user settings and update resolution, eg 300dpi or 118,11 ppc
+            for tag, value in zip(self.dpi_tags, (self.dpi_res_x, self.dpi_res_y, self.dpi_unit)):
+                command.append(f'{tag}={value}')
 
-            command.append(f'{tag}={dict_value}')
+        if self.update_from_excel:
+            # Update image tags from excel data
+            for tag, dict_value in zip(Exif.exiftool_tags, img_dict.values()):
+                if not dict_value:
+                    continue
+                command.append(f'{tag}={dict_value}')
 
         # Do not back up files
         command.append('-overwrite_original')
--- modules/widgets/message_box.py
+++ modules/widgets/message_box.py
@@ -36,3 +36,31 @@ class ExcelLoadFailedMsgBox(GenericMsgBox):
 
     def __init__(self, parent):
         super(ExcelLoadFailedMsgBox, self).__init__(parent, self.title, self.text, self.icon_key)
+
+
+class QuestionBox:
+    @staticmethod
+    def ask(parent,
+            title_txt: str = _('Frage'),
+            message: str = '',
+            ok_btn_txt: str=_('OK'),
+            abort_btn_txt: str= _('Abbrechen')
+            ) -> bool:
+        """ Pop-Up Warning Box ask to continue or break, returns False on abort """
+        msg_box = QMessageBox(QMessageBox.Question,
+                              title_txt, message, parent=parent)
+
+        msg_box.setStandardButtons(QMessageBox.Ok | QMessageBox.Abort)
+
+        msg_box.button(QMessageBox.Ok).setText(ok_btn_txt)
+        msg_box.button(QMessageBox.Abort).setText(abort_btn_txt)
+
+        # Block until answered
+        answer = msg_box.exec()
+
+        if answer == QMessageBox.Abort:
+            # User selected abort
+            return False
+
+        # User selected -Ok-, continue
+        return True
